28ce6d8df92ca9265b65c35bc3bc25144079c407
Raymond Rich-Fiondella <ray@Rays-MacBook-Pro.local>
2016-05-01 18:04:57 -0400

    add movie node

--- mNode.java
+++ mNode.java
@@ -1,52 +1,71 @@
-
-
+//movie node
 
 public class mNode {
-	private String name;
+	private String title;
 	private int release;
-	private
+	private int code;
+	private int rtScore;
+	private boolean inStock;
+	private Node right;
+	private Node left;
 
-	private Node next;
-	private String email;
 	//instance variables of the node class
-	public cNode(String name0, int cc, String email){
-		name = name0;
-		cc = cc0;
-		next = null;
-		email = email0;
+	public mNode(String title0, int release0, int code0, int rtScore0){
+		title = title0;
+		release = release0;
+		code = code0;
+		rtScore = rtScore0;
+		inStock = true;
+		right = null;
+		left = null;
 	}
-	// these are the methods that the node class uses
-	//to get the ssn of a node, just return its ssn.
-	public int getcc(){
-		return cc;
+
+	public int getRdate(){
+		return release;
 	}
-	// just return the name, which is a string
-	public String getName(){
-		return name;
+	public String getTitle(){
+		return title;
 	}
-	//the key is the last four digits of a ssn, so we can get it by
-	//returning the mod 10000 of the ssn
-	public int getKey(){
-		return  cc % 10000;
+	public int getRTscore(){
+		return  rtScore;
 	}
-	//to get the next node, just return what the pointer is pointing to
-	public Node getNext(){
-		return next;
+	public int getCode(){
+		return code;
 	}
+	public Node getRight(){
+		return right;
+	}
+	public boolean checkStock(){
+		return inStock;
+	}
+	public Node getLeft(){
+		return left;
+	}
+
+
+
 	//to set the name of a node, make its name = to name0
-	public void setName(String name0){
-		name = name0;
+	public void setTitle(String name0){
+		title = title0;
 	}
-	//to set the next node, make its pointer point to next0
-	public void setNext(Node next0){
-		next = next0;
+	public void setRdate(int release0){
+		release = release0;
 	}
-	//to set the ssn, make its ssn equal to ssn0
-	public void setSSN(int ssn0){
-		ssn = ssn0;
+	public void setRTscore(int rtScore0){
+		rtScore = rtScore0;
 	}
-	public void setEmail(String email0) {
-		email = email0;
+	public void setCode(int code0){
+		code = code0;
 	}
+	public void changeStatus(boolean inStock0){
+		inStock = inStock0;
+	}
+	public void setLeft(Node next){
+		left = next;
+	}
+	public void setRight(Node next){
+		right = next;
+	}
+
 }
 
