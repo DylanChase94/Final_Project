4081753edc7b125420c442c024e062ef928247e9
ptp7878 <pprigoda@conncoll.edu>
2016-05-02 13:30:33 -0400

    Wishlist class

---
+++ Wishlist.java
@@ -0,0 +1,48 @@
+/*
+PT Prigoda
+5/1/16
+Final Project
+*/
+
+public class Wishlist{
+	private mNode[] wishlist;
+	private int n;
+
+	public Wishlist(){
+		wishlist = new mNode[20];
+		n = 0;
+	}
+
+//user inputs wrong nmber check at menu
+	public void delete(int index){
+		n--;
+		for(int i = index; i < n; i++){
+			wishlist[i] = wishlist[i + 1];
+		}
+	}
+
+	public void insert(mNode newNode, int index){
+		n++;
+		for (int i = n; i >= index; i--){
+			wishlist[i] = wishlist[i-1];
+		}
+		wishlist[index-1] = newNode;
+	}
+
+	public void watchMovie(){
+		if (wishlist[0].checkStock()){
+			System.out.println("You are now watching your favorite movie.");
+		}
+		else{
+			System.out.println("Movie no longer avalible.");
+		}
+		delete(0);
+    }
+
+    public void printMovies(){
+    	for(int i = 0; i < n; i++){
+    		System.out.println(wishlist[i].getTitle());
+    		System.out.println(wishlist[i].getCode());
+    	}
+    }
+}
