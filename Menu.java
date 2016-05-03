07d110a9a43013ddf48a3673eed8e7268e8a884e
chuck04040 <cwilli18@conncoll.edu>
2016-05-03 19:45:07 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -25,6 +25,38 @@ public class Menu{
 	}
 }
 }
+import java.util.Scanner;
+
+public class bundle{
+
+	private String name;
+	private int grade;
+
+	public bundle(String name0, int grade0){
+        name = name0;
+        grade = grade0; 
+    }
+    public static void fillArray(student[] args){
+    	Scanner scan = new Scanner(System.in);  // Reading from System.in
+		for (int i=0; i<5; i++);
+		{
+    		 
+				System.out.println("Enter a name: ");
+				String s = scan.nextLine();
+				System.out.println("Enter a grade: ");
+				int x = scan.nextInt();
+				student stu1 = new student(s,x);
+				args[i] = stu1;
+    	}
+    }
+
+	public static void main(String[] args){
+		student[] myStudentArray = new student[5];
+		fillArray(myStudentArray);
+	}
+
+	
+}
 	
 }
 
