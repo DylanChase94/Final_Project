0e6af0b5e4c3cc163bd8c3b9a06a6b3bd2370c0f
rrichfio <rrichfio@conncoll.edu>
2016-05-09 21:24:56 -0400

    Add files via upload

--- MNode.java
+++ MNode.java
@@ -1,5 +1,5 @@
 //movie node
-
+import java.io.*;
 
 public class MNode implements java.io.Serializable{
 	private String title;
@@ -9,6 +9,8 @@ public class MNode implements java.io.Serializable{
 	private boolean inStock;
 	private MNode right;
 	private MNode left;
+	private MNode rightb;
+	private MNode leftb;
 
 	//instance variables of the node class
 	public MNode(String title0, int release0, int code0, int rtScore0){
@@ -19,6 +21,8 @@ public class MNode implements java.io.Serializable{
 		inStock = true;
 		right = null;
 		left = null;
+		rightb = null;
+		leftb = null;
 	}
 
 	public int getRdate(){
@@ -41,6 +45,12 @@ public class MNode implements java.io.Serializable{
 	}
 	public MNode getLeft(){
 		return left;
+	}
+	public MNode getLeftb(){
+		return leftb;
+	}
+	public MNode getRightb(){
+		return rightb;
 	}
 
 
@@ -67,16 +77,28 @@ public class MNode implements java.io.Serializable{
 	public void setRight(MNode next){
 		right = next;
 	}
+	public void setRightb(MNode next){
+		rightb = next;
+	}
+	public void setLeftb(MNode next){
+		leftb = left;
+	}
 	/*
-	public static void main(String[] args){
-		mNode x = new mNode("jack", 1234, 00000, 1);
+		public static void main(String[] args){
+		MNode x = new MNode("jack", 1234, 00000, 1);
 		System.out.println(x.getCode());
 		System.out.println(x.getRTscore());
 		System.out.println(x.getRdate());
-		mNode y = new mNode("jill", 1111, 123456, 2);
+		MNode y = new MNode("jill", 1111, 123456, 2);
 		x.setRight(y);
 		System.out.println(x.getRight().getTitle());
+		MNode z = new MNode("bob", 1212, 123123,2);
+		System.out.println(z.getTitle());
+		x.setRightb(z);
+		System.out.println(x.getRightb().getTitle());
+
+
 	}
 	*/
+	
 }
-
